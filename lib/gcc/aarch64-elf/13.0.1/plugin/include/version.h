f7db523651b9e3e5fe3f0255247595790847b36c
Leonardo Ledda <leonardoledda@gmail.com>
2023-03-17 20:12:08 +0100

    gcc-arm64: Import Furry GCC 20230317
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- lib/gcc/aarch64-elf/13.0.1/plugin/include/version.h
+++ lib/gcc/aarch64-elf/13.0.1/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "13.0.1 20230226 (Stable)"
+#define version_string "13.0.1 20230317 (Stable)"
 #define pkgversion_string "(Furry GCC) "
 
 /* This is the location of the online document giving instructions for
